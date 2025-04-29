3e24e53136207d45b105861af46219808e318e5e
Gamnraa <ltaz1466@gmail.com>
2025-04-29 11:20:35 -0400

    Wonkey voice + plus reintegrate talker
    Wisecracker will CRASH without talker and we want talker anyways

--- scripts/prefabs/willar.lua
+++ scripts/prefabs/willar.lua
@@ -2,7 +2,8 @@ local MakePlayerCharacter = require "prefabs/player_common"
 
 local assets = {
     Asset("SCRIPT", "scripts/prefabs/player_common.lua"),
-	Asset("ANIM", "anim/shadow_willar.zip")
+	Asset("ANIM", "anim/shadow_willar.zip"),
+	Asset("SOUND", "sound/webber.fsb"),
 }
 
 local prefabs = {
@@ -232,10 +233,10 @@ local master_postinit = function(inst)
     inst.starting_inventory = start_inv[TheNet:GetServerGameMode()] or start_inv.default
 	
 	-- choose which sounds this character will play
-	inst.soundsname = "willow"
+	inst.soundsname = "wonkey"
 	
 	-- Uncomment if "wathgrithr"(Wigfrid) or "webber" voice is used
-    --inst.talker_path_override = "dontstarve_DLC001/characters/"
+   	inst.talker_path_override = "monkeyisland/characters/"
 	
 	-- Stats	
 	inst.components.health:SetMaxHealth(150)
@@ -263,7 +264,6 @@ local master_postinit = function(inst)
 
 	inst.components.foodaffinity:AddPrefabAffinity("cave_banana", TUNING.AFFINITY_15_CALORIES_MED)
 
-	inst:RemoveComponent("talker")
 
 	-- Damage multiplier (optional)
     inst.components.combat.damagemultiplier = 1
