a026402ee3c5f57effe9836a40ecef02e3ee29b8
Gamnraa <ltaz1466@gmail.com>
2025-06-17 15:49:37 -0400

    Initial impl of blanket

---
+++ scripts/prefabs/willartapestry.lua
@@ -0,0 +1,95 @@
+local assets =
+{
+    Asset("ANIM", "anim/merm_king_carpet.zip"),
+    Asset("MINIMAP_IMAGE", "merm_king_carpet"),
+}
+
+local buff_monkey_attack = 1.15
+local buff_monkey_health = 50
+
+local function onpoweredup(inst)
+    --Heaven forgive me wdym this is the way you get every type of prefab in the world
+    local x,y,z = inst.Transform:GetWorldPosition()
+    for _, v in pairs(TheSim:FindEntities(x,y,z, 9009, nil, nil, {"monkey", "wonkey"})) do
+        if v:HasTag("player") then
+            Gram_UpdateMaxHealth(v, 10)
+            Gram_UpdateMaxSanity(v, 10)
+            Gram_UpdateMaxSanity(v, 10)
+        else
+            Gram_UpdateMaxHealth(v, buff_monkey_health)
+            v.components.combat.externaldamagemultipliers:SetModifier(v, buff_monkey_attack, "willartapestryactive")
+        end
+    end
+end
+
+local function onlosepower(inst)
+    local x,y,z = inst.Transform:GetWorldPosition()
+    for _, v in pairs(TheSim:FindEntities(x,y,z, 9009, nil, nil, {"monkey", "wonkey"})) do
+        if v:HasTag("player") then
+            Gram_UpdateMaxHealth(v, -10)
+            Gram_UpdateMaxSanity(v, -10)
+            Gram_UpdateMaxSanity(v, -10)
+        else
+            Gram_UpdateMaxHealth(v, -buff_monkey_health)
+            v.components.combat.externaldamagemultipliers:SetModifier(v, 1.00, "willartapestryactive")
+        end
+    end
+end
+
+local function ontimerdone(inst, data)
+    if data.name == "willartapestry" then
+        --Debuff monkeys
+        onlosepower(inst)
+        --make component accept materials again
+    end
+end
+
+local function onconstructed(inst, doer)
+end
+
+local function onremoved(inst)
+    onlosepower(inst)
+end
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+    inst.entity:AddMiniMapEntity()
+    inst.entity:AddNetwork()
+
+    inst.Transform:SetScale(0.9, 0.9, 0.9)
+
+    inst.MiniMapEntity:SetIcon("merm_king_carpet.png")
+
+    inst.AnimState:SetBank("merm_king_carpet")
+    inst.AnimState:SetBuild("merm_king_carpet")
+    inst.AnimState:PlayAnimation("idle", true)
+
+    inst.AnimState:SetOrientation( ANIM_ORIENTATION.OnGround )
+    inst.AnimState:SetLayer( LAYER_BACKGROUND )
+    inst.AnimState:SetSortOrder( 3 )
+
+    inst:AddTag("mermthrone")
+
+    inst.entity:SetPristine()
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("inspectable")
+
+    inst:AddComponent("lootdropper")
+
+    local workable = inst:AddComponent("workable")
+    workable:SetWorkAction(ACTIONS.HAMMER)
+    workable:SetWorkLeft(4)
+    workable:SetOnFinishCallback(onhammered_regular)
+   
+    MakeHauntableWork(inst)
+
+    inst:ListenForEvent("ondeconstructstructure", onremoved)
+    inst:ListenForEvent("onremove", onremoved)
+end
